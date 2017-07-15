cb343db1144ba40ee72639aaf0b73959ec9bc102
Bobby Lei <bobl53@gmail.comm>
2017-07-15 22:35:10 +0800

    Setting up tests

---
+++ test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,11 @@
+package endtoend.auctionsniper;
+
+import org.junit.Test;
+
+public class AuctionSniperEndToEndTest {
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+
+    }
+}
